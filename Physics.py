98016abc5af3afd0a30ff80d0001263875c7035f
Nikita Kostromin <86405899+KapaBlanka212@users.noreply.github.com>
2022-11-15 16:07:32 +0300

    feat(Physics): true E finded

--- Physics.py
+++ Physics.py
@@ -1,7 +1,5 @@
-from typing import List, Union, Tuple, NoReturn
 import numpy as np
 from cmath import exp, sqrt
-import matplotlib.pyplot as plt
 
 from MaterialConstants import StructureCONST
 CONST = StructureCONST(300)
@@ -10,58 +8,51 @@ CONST = StructureCONST(300)
 class Physics:
 
     @staticmethod
-    def k_in_qw(m: float, energy: float):
-        energy *= CONST.e
-        k_out = sqrt(2 * m * energy) / CONST.h_
-        return k_out
-
-    @staticmethod
-    def k_in_barrier(m: float, energy: float, potential: float):
+    def k_(mass_: float, energy: float, potential: float) -> float:
         energy *= CONST.e
         potential *= CONST.e
-        k_out = sqrt(2 * m * (energy - potential)) / CONST.h_
+        k_out = sqrt(2 * mass_ * (energy - potential)) / CONST.h_
         return k_out
 
     @staticmethod
-    def exp_body(k: float, a: float):
-        a *= 10 ** -9
-        out_body = 1j * k * a
+    def exp_body(k: float, a: float) -> float:
+        width = a * 10 ** (-9)
+        out_body = 1j * k * width
         return out_body
 
     @staticmethod
-    def qw_matrix(a: float, k: float):
+    def qw_matrix(a: float, k: float) -> np.ndarray:
         exp_body = Physics.exp_body(k, a)
-        out_matrix = np.array([[exp(exp_body), 0],
-                               [0, exp(-exp_body)]])
+        out_matrix = np.array([[exp(exp_body), 0.0],
+                               [0.0, exp(-exp_body)]], dtype=object)
         return out_matrix
 
     @staticmethod
-    def barrier_matrix(mass_: float, k: float):
+    def barrier_matrix(mass_: float, k: float) -> np.ndarray:
         out_matrix = np.array([[1, 1],
-                               [1j * k / mass_, -1j * k / mass_]])
+                               [(1j * k) / mass_, (-1j * k) / mass_]], dtype=object)
         return out_matrix
 
     @staticmethod
-    def inverse_barrier_matrix(mass_, k):
-        out_matrix = np.array([[1 / 2, - 1j * mass_ / (2 * k)],
-                               [1 / 2, 1j * mass_ / (2 * k)]])
+    def inverse_barrier_matrix(mass_: float, k: float) -> np.ndarray:
+        out_matrix = 1 / 2 * np.array([[1, mass_ / (1j * k)],
+                                       [1, - mass_ / (1j * k)]], dtype=object)
         return out_matrix
 
     @staticmethod
-    def transfer_matrix(energy: float):
-        # Describe barriers and wave vectors
+    def transfer_matrix(energy: float) -> float:
+        # Describe barriers and K
         U0, U1 = np.array(CONST.Eg_GaAs - CONST.Eg_AlGaAs)
-        k0 = k4 = Physics.k_in_barrier(CONST.m_AlGaAs[0], energy, U0)
-        k1 = k3 = Physics.k_in_qw(CONST.m_GaAs, energy)
-        k2 = Physics.k_in_barrier(CONST.m_AlGaAs[1], energy, U1)
+        k0 = k4 = Physics.k_(CONST.m_AlGaAs[0], energy, U0)
+        k1 = k3 = Physics.k_(CONST.m_GaAs, energy, 0.0)
+        k2 = Physics.k_(CONST.m_AlGaAs[1], energy, U1)
 
-        # Describe matrices
         m4_inv = Physics.inverse_barrier_matrix(CONST.m_AlGaAs[0], k4)
         m3 = Physics.barrier_matrix(CONST.m_GaAs, k3)
-        n2 = Physics.qw_matrix(CONST.WIDTH_FIRST_QW + CONST.WIDTH_SECOND_QW + CONST.WIDTH_BARRIERS, k1)
+        n2 = Physics.qw_matrix(CONST.WIDTH_SECOND_QW, k3)
         m3_inv = Physics.inverse_barrier_matrix(CONST.m_GaAs, k3)
         m2 = Physics.barrier_matrix(CONST.m_AlGaAs[1], k2)
-        n1 = Physics.qw_matrix(CONST.WIDTH_FIRST_QW + CONST.WIDTH_BARRIERS, k2)
+        n1 = Physics.qw_matrix(CONST.WIDTH_BARRIERS, k2)
         m2_inv = Physics.inverse_barrier_matrix(CONST.m_AlGaAs[1], k2)
         m1 = Physics.barrier_matrix(CONST.m_GaAs, k1)
         n0 = Physics.qw_matrix(CONST.WIDTH_FIRST_QW, k1)
