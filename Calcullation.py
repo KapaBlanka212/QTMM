c4e588a0c39aeb01a90f3436559ab871b2d614aa
Nikita Kostromin <86405899+KapaBlanka212@users.noreply.github.com>
2022-11-27 13:57:58 +0300

    feat(Calculation Physics): added finding wave fuction

--- Calcullation.py
+++ Calcullation.py
@@ -9,12 +9,12 @@ from Physics import Physics
 CONST = StructureCONST(300)
 
 
-def plot_t11_energy(steps, potential):
+def plot_t11_energy(step, potential):
     U = potential
     min_E = U[0] / 100
     max_E = U[0] * 0.999
-    root_e_find_space = np.geomspace(min_E, max_E, steps)
-    t11_element_value = [Physics.find_energy(energy).real for energy in root_e_find_space]
+    root_e_find_space = np.geomspace(min_E, max_E, step)
+    t11_element_value = [Physics.find_energy(energy)[1, 1].real for energy in root_e_find_space]
     figure_e = plt.figure()
     ax = figure_e.add_subplot()
     ax.set_ylabel('Value element T[2,2] ', fontsize=12)
@@ -24,34 +24,77 @@ def plot_t11_energy(steps, potential):
     plt.show()
 
 
-if __name__ == "__main__":
+def plot_conductive_band(energy, potential):
+    U0, U1 = potential
+    x = np.array([0.0,
+                  CONST.WIDTH_FIRST_QW,
+                  CONST.WIDTH_BARRIERS + CONST.WIDTH_FIRST_QW,
+                  CONST.WIDTH_SECOND_QW + CONST.WIDTH_FIRST_QW + CONST.WIDTH_BARRIERS,
+                  CONST.WIDTH_SECOND_QW + CONST.WIDTH_FIRST_QW + CONST.WIDTH_BARRIERS])
+    y = np.array([U0, 0.0, U1,   0.0, U0])
 
-    initial_guess = [0.07 * CONST.e, 0.16 * CONST.e, 0.28 * CONST.e]
-    eqn_energy = lambda e: Physics.find_energy(e).real
-    print(eqn_energy(0.0701 * CONST.e), eqn_energy(0.16 * CONST.e), eqn_energy(0.28 * CONST.e))
-    E = []
-    try:
-        for x0 in initial_guess:
-            energy_ans = newton(eqn_energy, x0)
-            E.append(energy_ans)
-    except:
-        Exception
-    print(E)
-    steps = 1000
-    z_min = -10 * 10 ** -9
-    z_max = 15 * 10 ** -9
-    z_space = np.linspace(z_min, z_max, steps)
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.step(x * 10 ** 9, y, linewidth=2, where='pre')
+    for ans in energy:
+        ax.plot([0.0 * 10 ** 9, (CONST.WIDTH_SECOND_QW + CONST.WIDTH_FIRST_QW + CONST.WIDTH_BARRIERS) * 10 ** 9],
+                [ans, ans])
+    ax.set_ylabel('Eg, eV', fontsize=15)
+    ax.set_xlabel('x, nm', fontsize=15)
+    plt.show()
 
-    wave_functon = np.copy([Physics.find_wave_function(0.16 * CONST.e, item) for item in z_space])
-    int = np.sqrt(quad(lambda x: np.abs(Physics.find_wave_function(0.16 * CONST.e, x)[0, :]) ** 2, z_min, z_max))
-    print(int[0])
 
-    figure_w = plt.figure()
-    ax = figure_w.add_subplot()
+def plot_wave_function(energy, z_array):
+    wave_functions = [np.copy([Physics.find_wave_function(energy_i, z) for z in z_space]) for energy_i in energy]
+    figure_e = plt.figure()
+    ax = figure_e.add_subplot()
     ax.set_ylabel('Wave Function', fontsize=12)
-    ax.set_xlabel('z, nm', fontsize=12)
-    ax.plot(z_space * 10 ** 9, wave_functon[:, 0], label='Full WF')
+    ax.set_xlabel('z, eV', fontsize=12)
+    for wave_function_i in range(len(wave_functions)):
+        ax.plot(z_array * 10 ** 9, wave_functions[wave_function_i][:, 0],
+                label=f"Wave Function for E = {energy[wave_function_i]}")
+    ax.plot(z_array * 10 ** 9, np.zeros(z_space.shape))
     ax.legend()
     plt.show()
+
+
+def plot_norm_wave_function(energy, z_array):
+    wave_functions_array = [np.copy([Physics.find_wave_function(energy_i, z) for z in z_space])
+                            for energy_i in energy]
+    wave_functions_sqr = [np.copy([np.abs(Physics.find_wave_function(energy_i, z)) ** 2 for z in z_space])
+                          for energy_i in energy]
+    integrals = [np.sqrt(simpson(wave_functions_sqr[i][:, 0], z_array, axis=0))
+                 for i in range(len(wave_functions_sqr))]
+    figure_e = plt.figure()
+    ax = figure_e.add_subplot()
+    ax.set_ylabel('Wave Function', fontsize=12)
+    ax.set_xlabel('z, eV', fontsize=12)
+    for wave_function_i in range(len(wave_functions_array)):
+        ax.plot(z_array * 10 ** 9, wave_functions_array[wave_function_i][:, 0] / integrals[wave_function_i],
+                label=f"Wave Function for E = {energy[wave_function_i]}")
+    ax.plot(z_array * 10 ** 9, np.zeros(z_space.shape))
+    ax.legend()
+    plt.savefig("WF")
+    plt.show()
+
+
+def norm_wave_function(wave_function_not_norm, z):
+    ints = np.sqrt(quad(wave_function_not_norm, z[0], z[-1]))
+    return ints
+
+
+if __name__ == "__main__":
+    initial_guess = [0.06, 0.16, 0.28]
+    steps = 1000
+    z_space = np.linspace(-20, 20, steps) * 10 ** -9
+    E = []
+    steps = 1000
+    for x0 in initial_guess:
+        ans = newton(lambda x: Physics.find_energy(x)[1, 1].real, x0)
+        E.append(ans)
+    
+    з
+    plot_wave_function(E, z_space)
+    plot_norm_wave_function(E, z_space)
 
 
--- Physics.py
+++ Physics.py
@@ -10,48 +10,45 @@ class Physics:
 
     @staticmethod
     def k_(mass_: float, energy: float, potential: float) -> float:
-        k_out = sqrt(2 * mass_ * (energy - potential)) / CONST.h_
+        E = energy * CONST.e
+        P = potential * CONST.e
+        k_out = sqrt(2 * mass_ * (E - P)) / CONST.h_
         return 1j * abs(k_out)
 
     @staticmethod
     def real_k(mass_: float, energy: float) -> float:
-        k_out = np.sqrt(2 * mass_ * energy) / CONST.h_
+        E = energy * CONST.e
+        k_out = np.sqrt(2 * mass_ * E) / CONST.h_
         return k_out
 
     @staticmethod
-    def exp_body(k: float, a: float) -> complex:
-        out_body = 1j * k * a
-        return out_body
-
-    @staticmethod
     def n_matrix(a: float, k: float) -> np.ndarray:
-        exp_body = Physics.exp_body(k, a)
-        out_matrix = np.array([[exp(exp_body), 0.0],
-                               [0.0, exp(-exp_body)]])
+        out_matrix = np.array([[exp(1j * k * a), 0.0],
+                               [0.0, exp(-1j * k * a)]], np.dtype(np.complex128))
         return out_matrix
 
     @staticmethod
     def n_z_matrix(z: float, z0: float, k: float) -> np.ndarray:
         out_matrix = np.array([[exp(1j * k * (z - z0)), 0.0],
-                               [0.0, exp(-1j * k * (z - z0))]])
+                               [0.0, exp(-1j * k * (z - z0))]], np.dtype(np.complex128))
         return out_matrix
 
     @staticmethod
     def m_matrix(mass_: float, k: float) -> np.ndarray:
         elements = 1j * k / mass_
         out_matrix = np.array([[1, 1],
-                               [elements, -elements]])
+                               [elements, -elements]], np.dtype(np.complex128))
         return out_matrix
 
     @staticmethod
     def inverse_m_matrix(mass_: float, k: float) -> np.ndarray:
         elements = 1j * mass_ / k
         out_matrix = 1 / 2 * np.array([[1, -elements],
-                                       [1, elements]])
+                                       [1, elements]], np.dtype(np.complex128))
         return out_matrix
 
     @staticmethod
-    def find_energy(energy: float) -> float:
+    def find_energy(energy: float) -> np.ndarray:
         # there we find E - energy eigenvalues
         U = CONST.U  # U0 = 0.44 eV, U1 = 0.33 eV
 
@@ -89,8 +86,7 @@ class Physics:
         t2 = n2 @ m2_inv @ m1
         t3 = n3 @ m3_inv @ m2
         full_t = m4_inv @ m3 @ t3 @ t2 @ t1
-
-        return full_t[1, 1]
+        return full_t
 
     @staticmethod
     def find_wave_function(energy: float, z: float) -> List:
@@ -125,17 +121,25 @@ class Physics:
         m4 = Physics.m_matrix(CONST.m_AlGaAs[0], k4)
         m4_inv = Physics.inverse_m_matrix(CONST.m_AlGaAs[0], k4)
 
+        n1_1 = Physics.n_matrix(CONST.WIDTH_FIRST_QW, k1)
+        n2_1 = Physics.n_matrix(CONST.WIDTH_BARRIERS, k2)
+        n3_1 = Physics.n_matrix(CONST.WIDTH_SECOND_QW, k3)
+
         n1 = Physics.n_z_matrix(z, z0, k1)
         n2 = Physics.n_z_matrix(z, z1, k2)
         n3 = Physics.n_z_matrix(z, z2, k3)
         n4 = Physics.n_z_matrix(z, z3, k4)
 
         C0 = np.array([[0], [1]])
+        t1 = n1_1 @ m1_inv @ m0
+        t2 = n2_1 @ m2_inv @ m1
+        t3 = n3_1 @ m3_inv @ m2
+
         C1 = n1 @ m1_inv @ m0 @ C0
-        C2 = n2 @ m2_inv @ m1 @ C1
-        C3 = n3 @ m3_inv @ m2 @ C2
-        C4 = n4 @ m4_inv @ m3 @ C3
-        C4[1] = 0.0
+        C2 = n2 @ m2_inv @ m1 @ t1 @ C0
+        C3 = n3 @ m3_inv @ m2 @ t2 @ t1 @ C0
+        C4 = n4 @ m4_inv @ m3 @ t3 @ t2 @ t1 @ C0
+
         if z < z0:
             return m0 @ (C0 * exp(-1j * k0 * z))
         elif z0 < z < z1:
